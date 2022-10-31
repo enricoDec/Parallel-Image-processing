a2f8b57bf0ce01b4277ee34b920e0d8fc51d2730
enricoDec <enricodechadarevian@gmail.com>
2022-10-31 12:40:07 +0100

    Project setup

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+/**
+ * @author : Enrico Gamil Toros
+ * Project name : Parallel-Image-processing
+ * @version : 1.0
+ * @since : 31.10.22
+ **/
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
