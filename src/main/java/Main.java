9e2b26cc5f5dbf3cd8279bc7b376872560d9d833
enricoDec <enricodechadarevian@gmail.com>
2022-11-01 12:46:54 +0100

    Test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,10 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.Buffer;
+
 /**
  * @author : Enrico Gamil Toros
  * Project name : Parallel-Image-processing
@@ -5,7 +12,10 @@
  * @since : 31.10.22
  **/
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        File file = new File("src/main/resources/images/animal/1.kitten_small.jpg");
+        BufferedImage image =  ImageIO.read(file);
+        image.getRGB(1,1);
 
     }
 }
